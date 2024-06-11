bc7d7ce4eb9e7f1782b30a19373b2fc12b9f7556
= <cabkebe02@gmail.com>
2024-06-11 10:58:06 +0000

    validators

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(public formBuilder: FormBuilder,
+    public loadingCtrl: LoadingController,
+    public authService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fullname: ['', Validators.required],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+      ]],
+      password: ['', Validators.required,
+        Validators.pattern("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/")
+      ]
+    })
   }
 
+  get errorControl(){
+    return this.registerForm.controls;
+  }
+
+  async signUp(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if (this.registerForm.valid) {
+      // const user = this.authService.registerUser(email,password);
+    }
+  }
 }
